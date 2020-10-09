0d2ae81be75aae5eceb3d019dae9664b9b91b06d
王珏 <wangjuesix@gmail.com>
2020-10-09 19:06:36 +0800

    解析视频mpd文件

--- server/server.js
+++ server/server.js
@@ -4,15 +4,59 @@ const static = require("koa-static")
 const xml2js = require("xml2js")
 const fs = require("promise-fs")
 const path = require("path")
+const assert = require("assert")
 let app = new Koa()
 
 let router = new Router()
 
+function readXml(path){
+    return new Promise((resolve, reject)=>{
+       fs.readFile(path).then(buffer=>{
+          xml2js.parseString(buffer.toString(), (err, xml)=>{
+              if(err){
+                  reject(err)
+              }else{
+                  resolve(xml)
+              }
+          })  
+       }, reject)
+    })
+   
+}
+
 router.get("/meta/:name",async ctx=>{
     const {name} = ctx.params
-    console.log("name",name)
-    let buffer = await fs.readFile(path.resolve("videos/", name+'_dash.mpd'))
-    ctx.body = buffer.toString()
+    let file = path.resolve("videos/", name+'_dash.mpd')
+    let xml = await readXml(file)
+   
+    let duration = 0
+
+    let arr = xml.MPD.$.mediaPresentationDuration.match(/\d+(\.\d+)?[A-Z]/g)
+    arr.forEach(s=>{
+        if(s.endsWith("H")){
+            duration+=parseFloat(s)*3600
+        }else if(s.endsWith("M")){
+            duration+=parseFloat(s)*60
+        }else if(s.endsWith("S")){
+            duration+=parseFloat(s)
+        }else{
+            assert(false, "unknow time" + s)
+        }
+    })
+
+    let {mimeType, codecs, width, height} = xml.MPD.Period[0].AdaptationSet[0].Representation[0].$
+    let List = xml.MPD.Period[0].AdaptationSet[0].Representation[0].SegmentList[0]
+    let segmentSize = List.$.duration
+    let segments = [
+        List.Initialization[0].$.range,
+        ...List.SegmentURL.map(segment=>segment.$.mediaRange)
+    ].map(item=>item.split("-").map(Number))
+    
+   
+    ctx.body = {
+        duration: Math.floor(duration),mimeType, codecs, width, height, segmentSize,
+        segments
+    }
 })
 
 
